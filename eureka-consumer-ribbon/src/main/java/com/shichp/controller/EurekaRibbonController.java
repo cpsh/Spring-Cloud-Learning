25ebbfe013479e9df0701c521d621160488ff15c
shichp <shichp@fenqi.im>
2017-12-05 20:51:24 +0800

    使用ribbon组件来进行对调用Eureka注册中心提供的服务的消费者，实现负载均衡机制

---
+++ eureka-consumer-ribbon/src/main/java/com/shichp/controller/EurekaRibbonController.java
@@ -0,0 +1,26 @@
+package com.shichp.controller;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.client.RestTemplate;
+
+/**
+ * Created by shichp on 2017-12-05.
+ */
+@RestController
+public class EurekaRibbonController {
+    @Autowired
+    private RestTemplate restTemplate;
+
+    @GetMapping("/ribbon")
+    public String ribbonClient() {
+        /*
+        请求的host位置并没有使用一个具体的IP地址和端口的形式，而是采用了服务名的方式组成.
+        Spring Cloud Ribbon有一个拦截器，它能够在这里进行实际调用的时候，自动的去选取服务实例，
+        并将实际要请求的IP地址和端口替换这里的服务名，从而完成服务接口的调用。
+         */
+        return restTemplate.getForObject("http://eureka-client/dc", String.class);
+    }
+
+}
