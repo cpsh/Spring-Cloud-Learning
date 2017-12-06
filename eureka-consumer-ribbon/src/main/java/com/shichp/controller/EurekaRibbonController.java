ed58b20c63a734151c8bb09a1f2faa159700b344
shichp <shichp@fenqi.im>
2017-12-06 15:08:36 +0800

    添加配置，支持ribbon超时重试机制

--- eureka-consumer-ribbon/src/main/java/com/shichp/controller/EurekaRibbonController.java
+++ eureka-consumer-ribbon/src/main/java/com/shichp/controller/EurekaRibbonController.java
@@ -20,6 +20,11 @@ public class EurekaRibbonController {
         Spring Cloud Ribbon有一个拦截器，它能够在这里进行实际调用的时候，自动的去选取服务实例，
         并将实际要请求的IP地址和端口替换这里的服务名，从而完成服务接口的调用。
          */
+
+        /*
+        实例发生了故障而该情况还没有被服务治理机制及时的发现和摘除，这时候客户端访问该节点的时候自然会失败。
+        为了构建更为健壮的应用系统，我们希望当请求失败的时候能够有一定策略的重试机制，而不是直接返回失败
+         */
         return restTemplate.getForObject("http://eureka-client/dc", String.class);
     }
 
